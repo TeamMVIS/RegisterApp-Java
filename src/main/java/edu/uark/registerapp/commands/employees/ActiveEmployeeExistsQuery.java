9028b9f1bc4b6d32de5785086eeca2a6eb73f54a
joiner-caleb <77632343+joiner-caleb@users.noreply.github.com>
2021-03-04 22:33:59 -0600

    Fixing ActiveEmployeeEsists

--- src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -1,13 +1,21 @@
 package edu.uark.registerapp.commands.employees;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
-public class ActiveEmployeeExistsQuery{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActiveEmployeeExistsQuery implements VoidCommandInterface{
     @Override
 	public void execute() {
-		if (!this.employeeRepository.existsByIsActive(true)) {
+		if (!this.EmployeeRepo.existsByIsActive(true)) {
 			throw new NotFoundException("Employee");
 		}
 	}
+	@Autowired
+	private EmployeeRepository EmployeeRepo;
 
 }
