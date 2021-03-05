0c5fdfd28704ae2dbd2f65be40084a45307e7c7b
joiner-caleb <77632343+joiner-caleb@users.noreply.github.com>
2021-03-04 21:52:59 -0600

    Fixed error and sign in works.

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	// TODO: Anything you want to do when the page is loaded?
+	const FormEmployeeID = document.getElementById("ID");
+	FormEmployeeID.focus();
+	FormEmployeeID.select();
 });
 
 function validateForm() {
-	// TODO: Validate the user input
-	return true;
+	const FormEmployeeID = document.getElementById("ID").value;
+	const FormEmployeePass = document.getElementById("Pass").value;
+	var Validated;
+	if(isNaN(FormEmployeeID) || FormEmployeeID <= 0) {
+		Validated = false;
+	}
+	else if(FormEmployeePass == "" || FormEmployeePass == null) {
+		Validated = false;
+	}
+	else {
+		Validated = true;
+	}
+	return Validated;
 }
